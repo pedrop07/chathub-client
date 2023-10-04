64bed9cb4227c4272b1b82e9e2c1a35619accb76
Pedro <pedrofulfaro@gmail.com>
2023-10-04 16:42:52 -0300

    feat: update Chat interface

--- src/interfaces/Chat.ts
+++ src/interfaces/Chat.ts
@@ -1,10 +1,12 @@
+import { Message } from "postcss";
 import { User } from "./User";
 
 export interface Chat {
   id: string;
   title: string;
+  description: string;
   owner: Partial<User>;
   members: Partial<User[]>;
-  messages: any;
+  messages: Message[];
   created_at: string;
 }
